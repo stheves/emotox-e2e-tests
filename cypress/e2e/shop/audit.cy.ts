99d1278b92ecd350956de71b901291d7790cd7c8
(IOC/PAP-DDM-CO) Theves Sascha <Sascha.Theves@bosch.io>
2022-10-26 09:59:01 +0200

    accept cookies in audit tests

--- cypress/e2e/shop/audit.cy.ts
+++ cypress/e2e/shop/audit.cy.ts
@@ -1,4 +1,8 @@
 describe('Audit', () => {
+    beforeEach(() => {
+        cy.visit('https://www.e-moto-x.de/shop/');
+        cy.clickCookiesBtn('Akzeptieren');
+    });
     it('passes the audits', () => {
         cy.lighthouse();
         cy.pa11y({ threshold: 999 });
